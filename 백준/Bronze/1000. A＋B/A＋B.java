0152b8a1bb865e57d4750e7047c2c0f5f1aaf081
rammeobu <162889554+rammeobu@users.noreply.github.com>
2024-03-30 12:18:23 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+    Scanner scanner = new Scanner(System.in);
+    int x,y,z;
+    x= scanner.nextInt();
+    y= scanner.nextInt();
+    z=x+y;
+    System.out.printf("%d",z);
+    }
+}
