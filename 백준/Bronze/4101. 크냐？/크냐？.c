961a0bcd4a9c5701f2b4f6665c23040ccb668845
rammeobu <162889554+rammeobu@users.noreply.github.com>
2024-06-07 15:58:40 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	int n, m;
+	while (1)
+	{
+		scanf("%d %d", &n, &m);
+		if (n == 0 && m == 0) {
+			break;
+		}
+		else if (n > m) {
+			printf("Yes\n");
+		}
+		else
+		{
+			printf("No\n");
+		}
+		
+	}
+}
