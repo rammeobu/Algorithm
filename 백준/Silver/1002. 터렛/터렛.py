f5fa7167a05d6a9810df5b7e3336789dbfb69269
_.Cloudhigh <162889554+rammeobu@users.noreply.github.com>
2025-09-09 18:33:39 +0900

    [Silver III] Title: 터렛, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,15 @@
+import math
+testcast = int(input())
+for _ in range(testcast):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    d = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    if d == 0 and r1 == r2:
+        print(-1)
+    elif d == 0 and r1 != r2:
+        print(0)
+    elif d == r1 + r2 or d == abs(r1 - r2):
+        print(1)
+    elif abs(r1 - r2) < d < (r1 + r2):
+        print(2)
+    else:
+        print(0)
